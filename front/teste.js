c4d9c07f0850fc09cade2d57712e4d3a39a41a53
Thais <86132203+thaisfsousa@users.noreply.github.com>
2022-11-18 13:05:23 -0300

    alteração do grid e teclado

--- front/teste.js
+++ front/teste.js
@@ -1,5 +1,53 @@
+const state = {
+  grid: Array(6)
+  .fill()
+  .map(() => Array(6).fill('A')),
+  currentRow: 0,
+  currentCol: 0,
+};
+
+function updateGrid(){
+  for (let i = 0; i < state.grid.length; i++){
+    for (let j = 0; j < state.grid[i].length; j++) {
+      const box = document.getElementById(`box${i}${j}`);
+      box.textContent = state.grid[i][j];
+    }
+  }
+
+}
+
+function drawBox(container, row, col, letter = ''){
+const box = document.createElement('div');
+box.className = 'box';
+box.id = `box${row}${col}`;
+box.textContent = letter;
+
+container.appendChild(box);
+return box;
+}
+
+function drawGrid(container){
+  const grid = document.createElement('div');
+  grid.className = 'grid';
+  for (let i = 0; i < 6; i++){
+    for(let j = 0; j < 5; j++){
+    drawBox(grid, i, j);
+    }
+  }
+  container.appendChild(grid);
+}
+
+function startup() {
+  const game = document.getElementById('game');
+  drawGrid(game);
+  state.grid = Array(6)
+    .fill()
+    .map(() => Array(6).fill('A'));
+    updateGrid();
+}
+
+startup();
+
 function put_value(value) {
-  let x = document.getElementById("botao").value;
    document.getElementById("param_1").innerHTML = value;
-   console.log(value);
  };
